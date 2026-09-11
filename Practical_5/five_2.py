7af4e142d8d27d2e4b2b1071b33c6e4f5755cab4
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-09-11 15:05:01 +0530

    2 add

---
+++ Practical_5/five_2.py
@@ -0,0 +1,12 @@
+# 2. Gaussian Filter
+
+import cv2
+import numpy
+# using imread()
+img = cv2.imread("img1.jpg")
+dst = cv2.GaussianBlur(img, (5, 5), cv2.BORDER_DEFAULT)
+
+cv2.imshow('CS25D010', numpy.hstack((img, dst)))
+cv2.waitKey(0);
+cv2.destroyAllWindows();
+cv2.waitKey(1)
